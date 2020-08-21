174411faee65504633360e1c305de5cf4aa107df
felipe_c_rodrigues <felipe.cavalcante.rod@gmail.com>
2020-08-20 21:19:42 -0300

    estrutura inicial projeto

---
+++ src/main/java/br/com/fiap/test/SpringTest.java
@@ -0,0 +1,16 @@
+package br.com.fiap.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan("br.com.fiap") 
+public class SpringTest
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringTest.class, args);
+    }
+}
